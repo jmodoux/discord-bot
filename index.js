bb1f5ee353bf63acee912452f1cd0b0cc04ae901
jmodoux <josua.modoux@gmail.com>
2018-01-21 19:34:09 +0100

    [FEAT] dontAtMe working

--- index.js
+++ index.js
@@ -7,21 +7,16 @@ const youtube_key = 'AIzaSyBVbd6crFeK6qeryLldrM1Ip-kQml72rVU';
 
 // Commands
 const MusicBot = require('discord.js-musicbot-addon');
+const Command = require('./commands/command')
+
 const music = new MusicBot(client, {
     disableLoop: true,
 
     youtubeKey: youtube_key
 });
+const command = new Command(client);
+
 
 // Login bot
 client.login(bot_token);
-
-// Function call when someone send a message
-client.on('message', function (message) {
-    console.log(message.content);
-
-    //let commandUsed =
-    //Music.parse(message);
-})
-
 
